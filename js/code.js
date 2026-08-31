68636d576b20db9e8fdeb8d5a088a2367796dfb1
Felipe <felipe_oliveira2019@hotmail.com>
2026-08-31 16:38:23 -0300

    automizando funções e redundancias

--- js/code.js
+++ js/code.js
@@ -3,17 +3,46 @@ const insertSalario = document.querySelector('.btn_salario')
 
 
 // Funções
-CallSalario = () => {
-    const salarioModal = document.createElement("div")
-    const salarioModalText = document.createTextNode("Adicione o seu salario")
-    console.log(salarioModal)
-    salarioModal.appendChild(salarioModalText)
-    salarioModal.classList.add("salaryModal")
+ModalSalario = () => {
+    // variaveis
+
+    // Modal & Body
     const body = document.querySelector("body")
+    const salarioModal = document.createElement("div")
+    const contentModal = document.createElement("div")
+
+
+    // Content
+    const content = {
+        close: document.createElement("span"),
+        salarioModalText: document.createTextNode("Adicione o seu salario: "),
+        salarioModalTextBox: document.createElement("input"),
+        comissaoModalText: document.createTextNode("Adcione comissão: "),
+        comissaoModalTextBox: document.createElement("input"),
+        submit: document.createElement("input"),
+    }
+
+
+
+    // inserção de conteúdo das divs criadas
+    content.salarioModalTextBox.type = "text"
+    content.close.textContent = "X"
+    content.submit.type = "submit"
+
+    // inserção nas respectivas divs
     body.appendChild(salarioModal)
+    salarioModal.appendChild(contentModal)
+    Object.values(content).forEach((data) => {
+        contentModal.appendChild(data)
+    })
+
+    // inserção de classes (estilazação)
+    salarioModal.classList.add("salaryModal")
+    contentModal.classList.add("contentModal")
+        // console.log(content)
 }
 
 // EventListener
 insertSalario.addEventListener('click', () => {
-    CallSalario()
+    ModalSalario()
 })
